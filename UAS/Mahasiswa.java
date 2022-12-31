69a34c6a51d1603c03f29ee6be5460ded49357e3
IndsPtrS <108479618+Indsptr@users.noreply.github.com>
2022-12-31 17:23:08 +0700

    Add files via upload

---
+++ UAS/Mahasiswa.java
@@ -0,0 +1,20 @@
+public class Mahasiswa {
+    private long nim;
+    private String nama;
+
+    public void setNim(long value){
+        this.nim = value;
+    } 
+
+    public long getNim(){
+        return this.nim;
+    }
+
+    public void setNama(String value){
+        this.nama = value;
+    }
+
+    public String getNama(){
+        return this.nama;
+    }
+}
